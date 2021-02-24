78f0f93f52b379ceffa8016a0293f7d433db3754
romasinha <sinha.roma99@gmail.com>
2021-02-24 23:58:19 +0530

    bmi

---
+++ BMI.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+
+int main(){
+    int t, h, w;
+    double bmi;
+    cin>>t;
+    while(t--){
+        cin>>w;
+        cin>>h;
+        bmi = double(w/(h*h));
+        cout<<bmi<<endl;
+        
+        if(bmi <= 18){
+            cout<<"1"<<endl;
+        }
+        else if(bmi >= 19 && bmi <= 24){
+            cout<<"2"<<endl;
+        }
+        else if(bmi >= 25 && bmi <= 29){
+            cout<<"3"<<endl;
+        }
+        else if(bmi>=30) cout<<"4"<<endl; 
+    }
+}
---
+++ bmassi.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main(){
+    int h, w;
+    double bmi;
+    cin>>w;
+    cin>>h;
+
+
+    bmi = double(w/(h*h));
+    cout<<bmi<<endl;
+}
