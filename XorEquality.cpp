aa576b4c801c6f8c23d3270231d8290f62dcde87
romasinha <sinha.roma99@gmail.com>
2021-05-08 17:29:52 +0530

    mayc

---
+++ XorEquality.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define ll long long 
+
+ll fastModExp(ll a, ll b, ll m){
+    ll res = 1;
+    while(b>0){
+        if(b&1){
+            res = (res*a)%m;
+        }
+        a = (a*a)%m;
+        b = b>>1;
+    }
+    return res;
+}
+
+int main(){
+    long long int t;
+    cin>>t;
+    while(t--){
+        long long int n,a, b, x, m;
+        x = pow(10, 9);
+        m = x+7;
+        cin>>n;
+        cout<<fastModExp(2, n-1, x )<<endl;
+        
+    }
+    return 0;
+    
+}
